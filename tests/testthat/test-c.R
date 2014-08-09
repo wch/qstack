4594598b6a941fcdd3f8d106edb035479c66a6f3
Winston Chang <winston@stdout.org>
2014-08-09 01:05:00 -0500

    Add tests for push_list and append_list

--- tests/testthat/test-c.R
+++ tests/testthat/test-c.R
@@ -39,6 +39,33 @@ test_that("push_", {
 })
 
 
+test_that("push_list_", {
+  # Error if lst isn't a list
+  expect_error(push_list_(NULL, 1))
+  expect_error(push_list_(pairlist(1), 2))
+  expect_error(push_list_(pairlist(1, 2), pairlist(1,2)))
+
+  # Starting with empty pairlist
+  expect_identical(push_list_(NULL, NULL), NULL)
+  expect_identical(push_list_(NULL, list()), NULL)
+  expect_identical(push_list_(NULL, list(1)), pairlist(1))
+  expect_identical(push_list_(NULL, list(1,2)), pairlist(2,1))
+
+  # Starting with a real pairlist
+  p <- pairlist(1)
+  expect_identical(push_list_(p, NULL), p)
+  new_p <- push_list_(p, list(2,3))
+  expect_identical(new_p, pairlist(3,2,1))
+  expect_identical(address(cdr_(cdr_(new_p))), address(p))
+
+  p <- pairlist(2,1)
+  expect_identical(push_list_(p, NULL), p)
+  new_p <- push_list_(p, list(3,4))
+  expect_identical(new_p, pairlist(4,3,2,1))
+  expect_identical(address(cdr_(cdr_(new_p))), address(p))
+})
+
+
 test_that("append_", {
   expect_identical(append_(NULL, NULL), pairlist(NULL))
 
@@ -57,4 +84,31 @@ test_that("append_", {
   p <- pairlist(1,2)
   expect_identical(append_(p, 3), pairlist(3))  # Modifies in place and returns last
   expect_identical(p, pairlist(1,2,3))          # p is modified in place
+})
+
+
+test_that("append_list_", {
+  # Error if x is empty pairlist
+  expect_error(append_list_(NULL, NULL))
+  expect_error(append_list_(NULL, 1))
+  expect_error(append_list_(NULL, list(1)))
+
+  # Error if lst isn't a list
+  expect_error(append_list_(NULL, 1))
+  expect_error(append_list_(pairlist(1), 2))
+  expect_error(append_list_(pairlist(1, 2), pairlist(1,2)))
+
+
+  # Starting with a real pairlist
+  p <- pairlist(1)
+  expect_identical(append_list_(p, NULL), p)
+  last_p <- append_list_(p, list(2,3))
+  expect_identical(p, pairlist(1,2,3))
+  expect_identical(address(cdr_(cdr_(p))), address(last_p))
+
+  p <- pairlist(1,2)
+  expect_identical(append_list_(p, NULL), cdr_(p))
+  last_p <- append_list_(p, list(3,4))
+  expect_identical(p, pairlist(1,2,3,4))
+  expect_identical(address(cdr_(cdr_(cdr_(p)))), address(last_p))
 })
